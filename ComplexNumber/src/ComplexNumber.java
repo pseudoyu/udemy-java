913b32b36e9852c654ad730fa0c29ad4289d69bf
pseudoyu <pseudoyu@connect.hku.hk>
2021-01-05 00:31:44 +0800

    Section6 Exercises Done

---
+++ ComplexNumber/src/ComplexNumber.java
@@ -0,0 +1,39 @@
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(ComplexNumber cn) {
+        this.real += cn.real;
+        this.imaginary += cn.imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void subtract(ComplexNumber cn) {
+        this.real -= cn.real;
+        this.imaginary -= cn.imaginary;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+}
---
+++ inheritanceChallengeOne/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+    }
+}
