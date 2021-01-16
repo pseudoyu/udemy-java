f87919b567623dcaaf8e96e29ce1230f3153badd
pseudoyu <pseudoyu@connect.hku.hk>
2021-01-16 17:45:51 +0800

    Section12 Exercises Done

---
+++ Collections/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+        theatre.getSeats();
+    }
+}
---
+++ Collections/src/Theatre.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+/**
+ * @author hexarthur
+ */
+public class Theatre {
+
+    private final String theatreName;
+    private List<Seat> seats = new ArrayList<>();
+
+}
